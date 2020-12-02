c1fe75250c0e10ec525d4fd4ea8fd2e9fb24ff95
marial711 <marialeyva782@gmail.com>
2020-12-02 13:17:26 -0500

    added more values to csv

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 d3.dsv(',','PRECAVG.csv', function(d){
     return{
-        avg:+d.Value,
-        year:+d.Date
+        avg:+d.Value,//Average precipitation rates.
+        year:+d.Year, //Data is set in the month of May
+        temp:+d.Temp, //Average temp that month
+        DSI:+d.DSI //Drought Severity Index
     };
 }).then(function (data){
-    console.log(data);
+    console.log();
 })
