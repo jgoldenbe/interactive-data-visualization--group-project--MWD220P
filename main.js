0d5095583687d7caa85edf609850c0cfc10ef565
marial711 <marialeyva782@gmail.com>
2020-12-03 14:36:21 -0500

    Created into a radial bar chart

--- main.js
+++ main.js
@@ -11,35 +11,36 @@ d3.dsv(',','PRECAVG.csv', function(d){
                         //and DSI. Different colors can indicate DSI levels.
     var margin = {top: 20, right: 20, bottom: 20, left: 20},
         width = 1300 - margin.left - margin.right,
-        height = 600 - margin.top - margin.bottom;
+        height = 600 - margin.top - margin.bottom,
+        innerRadius= 80,
+        outerRadius = Math.min(width, height) / 2;
 
-    var svg = d3.select("body").append("svg")
+    var svg = d3.select("body")
+        .append("svg")
         .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom);
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + (height/2+100)+ ")");
 
     var xscale = d3.scaleBand()
-        .range([0, width])
+        .range([0, (2* Math.PI)])
+        .align(0)
         .domain(data.map(function(d){ return d.year; }))
-        .padding(2);
-    var yscale  =d3.scaleLinear()
+    var yscale  =d3.scaleRadial()
         .domain([0, 15])
-        .range([height, 0]);
-    svg.append("g")
-        .call(d3.axisRight(yscale));
+        .range([innerRadius, outerRadius]);
 
-    svg.selectAll("mybar")
+    svg.append("g")
+        .selectAll("path")
         .data(data)
-        .enter().append("rect")
-        .attr("fill", "#71c9f5")
-        .attr("x", function(d){ return xscale(d.year);})
-        .attr("width", xscale.bandwidth())
-        .attr("y", function(d){ return yscale(d.avg);})
-        .attr("height", function(d) { return height - yscale(d.avg);})
-
-    svg.append("g")
-        .attr("transform" , "translate(0,"+ height + ")")
-        .call(d3.axisBottom(xscale))
-        .selectAll("text")
-        .attr("transform", "translate(-10,0)rotate(-45)")
-        .style("text-anchor", "end");
+        .enter()
+        .append("path")
+        .attr("fill", "#69b3a2")
+        .attr("d", d3.arc()
+            .innerRadius(innerRadius)
+            .outerRadius(function(d){ return yscale(d['avg']); })
+            .startAngle(function(d) { return xscale(d.year); })
+            .endAngle(function(d) { return xscale(d.year) + xscale.bandwidth(); })
+                .padAngle(0.01)
+                .padRadius(innerRadius))
 });
